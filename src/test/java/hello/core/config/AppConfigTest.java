059adfbf893b5cf2757d16944b1419a92fc9cfd1
Domaksaramu <jewon945@gmail.com>
2022-05-09 22:50:48 +0900

    feat: 현재 수저아항 적용

---
+++ src/test/java/hello/core/config/AppConfigTest.java
@@ -0,0 +1,27 @@
+package hello.core.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.nio.charset.Charset;
+
+public class AppConfigTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        System.out.println("defaultCharset : " + Charset.defaultCharset().displayName());
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
